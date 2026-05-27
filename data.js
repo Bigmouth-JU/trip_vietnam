51300f2183416448cce243ace7a3622a4911a9be
Bemeup-JU <bluegn80@gmail.com>
2026-05-27 17:21:58 +0700

    09

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const RESTAURANTS_DATA = [
     "category": "관광객이라면 가성비와 갬성을 한번에!",
     "name": "짜까탕롱",
     "englishName": "Chả Cá Thăng Long",
-    "shortIntro": "테이블에서 노릇하게 볶아내는 하노이 명물 가물치 구이",
+    "shortIntro": "하노이의 명물 요리인 가물치 구이 '짜까(Chả Cá)' 전문점입니다. ...",
     "phone": "",
     "googleMapLink": "https://maps.app.goo.gl/HwShqpYcAyTzC9WE7",
     "intro": "하노이의 명물 요리인 가물치 구이 '짜까(Chả Cá)' 전문점입니다. 테이블 위에서 딜, 파와 함께 노릇하게 볶은 생선 살을 땅콩, 쌀국수 면, 특제 소스와 곁들여 먹는 독특한 경험을 할 수 있습니다. (처음 가는 사람은 찾기 어려우니 사진을 꼭 확인하세요! 6B P. Đường Thành, Cửa Đông, Hoàn Kiếm, Hà Nội 000084)",
@@ -13,8 +13,8 @@ const RESTAURANTS_DATA = [
       "생선 위장 볶음 (Lòng Cá)",
       "해산물 스프링 롤"
     ],
-    "rating": 4.0,
-    "reviewsCount": "",
+    "rating": 4.6,
+    "reviewsCount": "5,277",
     "tripAdvisorCount": "1.8천",
     "address": "6B Đường Thành, Cửa Đông, Hoàn Kiếm, Hà Nội",
     "hours": "오전 10:30~오후 9:00",
@@ -57,7 +57,7 @@ const RESTAURANTS_DATA = [
     "category": "관광객이라면 가성비와 갬성을 한번에!",
     "name": "꽌안응온",
     "englishName": "Quán Ăn Ngon",
-    "shortIntro": "야외 정원 분위기에서 깔끔하고 다양하게 즐기는 전통 요리",
+    "shortIntro": "베트남 전역의 다양한 길거리 음식과 전통 요리를 한곳에서 맛볼 수 있는 ...",
     "phone": "+84 903 246 963",
     "googleMapLink": "https://maps.google.com/?cid=5162517836559873849",
     "intro": "베트남 전역의 다양한 길거리 음식과 전통 요리를 한곳에서 맛볼 수 있는 곳입니다. 넓은 야외 정원과 전통 가옥 스타일의 인테리어가 어우러져 활기차면서도 이국적인 분위기를 느낄 수 있어 관광객들에게 인기가 많습니다.",
@@ -66,7 +66,7 @@ const RESTAURANTS_DATA = [
       "분짜 (Bún Chả)",
       "넴 란 (Nem Rán)"
     ],
-    "rating": 4.0,
+    "rating": 4.2,
     "reviewsCount": "4,471",
     "tripAdvisorCount": "4.2천",
     "address": "18 Phan Bội Châu, Cửa Nam, Hoàn Kiếm, Hà Nội",
@@ -111,7 +111,7 @@ const RESTAURANTS_DATA = [
     "category": "관광객이라면 가성비와 갬성을 한번에!",
     "name": "잠바 가든",
     "englishName": "Zamba Garden",
-    "shortIntro": "도심 속 정원 테라스에서 수제 맥주와 함께 즐기는 자이언트 폭립",
+    "shortIntro": "도심 속에서 활기찬 야외 정원 분위기를 즐길 수 있는 바베큐 전문점입니다...",
     "phone": "+84 901 676 969",
     "googleMapLink": "https://maps.app.goo.gl/sPsQkNUVHfsKr17J9",
     "intro": "도심 속에서 활기찬 야외 정원 분위기를 즐길 수 있는 바베큐 전문점입니다. 시그니처 메뉴인 큼직한 폭립을 비롯한 다양한 구이 요리에 시원한 맥주를 곁들이기 제격입니다.",
@@ -120,7 +120,7 @@ const RESTAURANTS_DATA = [
       "소고기 스테이크",
       "그릴드 소시지 플래터"
     ],
-    "rating": 4.0,
+    "rating": 4.2,
     "reviewsCount": "773",
     "tripAdvisorCount": "120",
     "address": "5 Yên Lãng, Trung Liệt, Đống Đa, Hà Nội",
@@ -163,9 +163,9 @@ const RESTAURANTS_DATA = [
   {
     "id": 4,
     "category": "관광객이라면 가성비와 갬성을 한번에!",
-    "name": "멧 1",
-    "englishName": "MẸT, Vietnamese restaurant & Vegetarian Food 1",
-    "shortIntro": "대나무 쟁반에 정갈하게 담겨 나오는 가성비 최고의 올드쿼터 맛집",
+    "name": "멧",
+    "englishName": "MẸT Vietnamese restaurant",
+    "shortIntro": "하노이 올드쿼터에 위치한 베트남 전통 및 채식 요리 전문점입니다. 대나무...",
     "phone": "+84 985 627 976",
     "googleMapLink": "https://maps.app.goo.gl/LwvdvYbzpWr6BdCZ8",
     "intro": "하노이 올드쿼터에 위치한 베트남 전통 및 채식 요리 전문점입니다. 대나무 바구니에 정갈하게 담겨 나오는 '멧(Mẹt)' 세트가 시그니처이며, 다양한 베트남 음식을 합리적인 가격에 즐길 수 있어 여행객들에게 인기가 높습니다.",
@@ -174,52 +174,94 @@ const RESTAURANTS_DATA = [
       "반세오 (Bánh Xèo)",
       "스프링롤 세트"
     ],
-    "rating": 5.0,
+    "rating": 4.9,
     "reviewsCount": "29,075",
-    "tripAdvisorCount": "280",
-    "address": "110 Hàng Bạc, Hàng Buồm, Hoàn Kiếm, Hà Nội",
+    "tripAdvisorCount": "",
+    "address": "",
     "hours": "오전 10:00~오후 10:30",
-    "michelinBadge": "Local Favorite",
-    "tags": [
-      "#올드쿼터",
-      "#멧세트",
-      "#채식옵션",
-      "#가성비맛집"
-    ],
-    "images": [
-      "https://metvietnameserestaurant.com/wp-content/uploads/2024/09/Thiet-ke-chua-co-ten-16-1.jpg",
-      "https://metvietnameserestaurant.com/wp-content/uploads/2025/05/menu-met-6-e1748568587346-1920x1528.jpg",
-      "https://ak-d.tripcdn.com/images/0104z12000kg7n61nFD3A_C_760_506_R5.jpg?proc=autoorient",
-      "https://ak-d.tripcdn.com/images/0104212000rrc67sa74C9_C_760_506_R5.jpg?proc=autoorient",
-      "https://ak-d.tripcdn.com/images/1tx2712000kg7fn1p2FD5_C_760_506_R5.jpg?proc=autoorient"
-    ],
-    "reviews": [
-      {
-        "username": "HanoiVibe",
-        "rating": 5,
-        "text": "대나무 쟁반에 정갈하게 플레이팅 된 전통 로컬 요리들이 아주 맛있었습니다. 다양한 음식을 부담 없이 일행과 여러 개 시켜 쉐어했습니다.",
-        "date": "2026-05-24"
-      },
-      {
-        "username": "배낭여행러",
-        "rating": 5,
-        "text": "채식주의자 메뉴도 다양해서 가볍고 건강하게 식사할 수 있습니다. 특히 반세오랑 튀긴 스프링롤이 바삭바삭하니 입에 매우 잘 맞았습니다.",
-        "date": "2026-05-18"
-      },
-      {
-        "username": "친절한설명",
-        "rating": 4,
-        "text": "하노이 올드쿼터에 있는 멧 매장들 중에 여기 5호점이 제일 깔끔한 것 같습니다. 가성비 좋은 하노이 로컬 맛집으로 강추합니다.",
-        "date": "2026-05-12"
-      }
-    ]
+    "michelinBadge": "",
+    "tags": [],
+    "images": [],
+    "reviews": []
   },
   {
     "id": 5,
+    "category": "관광객이라면 가성비와 갬성을 한번에!",
+    "name": "분차 타 하노이",
+    "englishName": "Bun Cha Ta Hanoi",
+    "shortIntro": "하노이 올드쿼터의 깔끔한 분짜 전문점으로, 외국인들에게도 인기가 많고 가...",
+    "phone": "+84 966848389",
+    "googleMapLink": "https://maps.app.goo.gl/3UQTLkYXtiCQSzDJA",
+    "intro": "하노이 올드쿼터의 깔끔한 분짜 전문점으로, 외국인들에게도 인기가 많고 가성비가 훌륭합니다.",
+    "menu": [
+      "분짜",
+      "넴",
+      "하노이 맥주"
+    ],
+    "rating": 4.4,
+    "reviewsCount": "5,049",
+    "tripAdvisorCount": "",
+    "address": "",
+    "hours": "오전 8:00~오후 10:00",
+    "michelinBadge": "",
+    "tags": [],
+    "images": [],
+    "reviews": []
+  },
+  {
+    "id": 6,
+    "category": "관광객이라면 가성비와 갬성을 한번에!",
+    "name": "피자 4P's 장띠엔",
+    "englishName": "Pizza 4P's Trang Tien",
+    "shortIntro": "베트남 최고의 피자 맛집으로, 화덕에서 구운 피자와 수제 치즈의 풍미가 ...",
+    "phone": "+84 1900 6043",
+    "googleMapLink": "https://maps.app.goo.gl/dBa66MSujhtgKUZE9",
+    "intro": "베트남 최고의 피자 맛집으로, 화덕에서 구운 피자와 수제 치즈의 풍미가 예술입니다.",
+    "menu": [
+      "부라타 치즈 피자",
+      "크랩 파스타",
+      "콰트로 포르마지 피자"
+    ],
+    "rating": 4.9,
+    "reviewsCount": "23,420",
+    "tripAdvisorCount": "",
+    "address": "",
+    "hours": "오전 11:00~오후 10:00",
+    "michelinBadge": "",
+    "tags": [],
+    "images": [],
+    "reviews": []
+  },
+  {
+    "id": 7,
+    "category": "관광객이라면 가성비와 갬성을 한번에!",
+    "name": "뉴 데이 레스토랑",
+    "englishName": "New Day Restaurant",
+    "shortIntro": "합리적인 가격에 정통 베트남 가정식을 다양하게 즐길 수 있어 여행자들 사...",
+    "phone": "+84 24 3828 0315",
+    "googleMapLink": "https://maps.app.goo.gl/535Yhdpbyfg6tHR99",
+    "intro": "합리적인 가격에 정통 베트남 가정식을 다양하게 즐길 수 있어 여행자들 사이에서 유명합니다.",
+    "menu": [
+      "스프링롤",
+      "소고기 볶음밥",
+      "갈릭 모닝글로리"
+    ],
+    "rating": 4.3,
+    "reviewsCount": "2,679",
+    "tripAdvisorCount": "",
+    "address": "",
+    "hours": "오전 10:30~오후 10:00",
+    "michelinBadge": "",
+    "tags": [],
+    "images": [],
+    "reviews": []
+  },
+  {
+    "id": 8,
     "category": "베트남만의 고급 로컬 다이닝",
     "name": "짜오반",
     "englishName": "Chào Bạn",
-    "shortIntro": "아늑한 서호 골목 빌라에서 즐기는 격조 높은 현대식 로컬 다이닝",
+    "shortIntro": "서호(Tây Hồ) 지역의 조용하고 운치 있는 빌라에 자리 잡은 베트남 ...",
     "phone": "+84 24 3552 8028",
     "googleMapLink": "https://maps.app.goo.gl/tq53AdfPzZCYW8bB8",
     "intro": "서호(Tây Hồ) 지역의 조용하고 운치 있는 빌라에 자리 잡은 베트남 전통 요리 전문점입니다. 미쉐린 가이드 빕 구르망에 선정될 만큼 훌륭한 맛과 우아한 분위기를 자랑합니다.",
@@ -228,7 +270,7 @@ const RESTAURANTS_DATA = [
       "베트남식 소고기 스튜 (Bò Sốt Vang)",
       "레몬그라스 돼지고기 구이"
     ],
-    "rating": 5.0,
+    "rating": 4.8,
     "reviewsCount": "1,188",
     "tripAdvisorCount": "95",
     "address": "Villa 28, Alley 11, Tô Ngọc Vân, Quảng An, Tây Hồ, Hà Nội",
@@ -270,11 +312,11 @@ const RESTAURANTS_DATA = [
     ]
   },
   {
-    "id": 6,
+    "id": 9,
     "category": "베트남만의 고급 로컬 다이닝",
     "name": "떰비",
     "englishName": "Tầm Vị",
-    "shortIntro": "고즈넉한 고가구 목조 가옥에서 맛보는 미쉐린 1스타 전통 가정식",
+    "shortIntro": "오래된 베트남 목조 주택의 고풍스러운 인테리어가 돋보이는 미쉐린 1스타 ...",
     "phone": "+84 966 323 131",
     "googleMapLink": "https://maps.google.com/?cid=4305134930746919990",
     "intro": "오래된 베트남 목조 주택의 고풍스러운 인테리어가 돋보이는 미쉐린 1스타 레스토랑입니다. 북부 지방 특유의 섬세한 전통 가정식을 매우 정성스럽고 고급스럽게 차려냅니다.",
@@ -287,7 +329,7 @@ const RESTAURANTS_DATA = [
     "reviewsCount": "3,570",
     "tripAdvisorCount": "410",
     "address": "4b Yên Thế, Văn Miếu, Đống Đa, Hà Nội",
-    "hours": "오전 11:00\\~오후 2:30<br>오후 5:00\\~9:30",
+    "hours": "오전 11:00~오후 9:30",
     "michelinBadge": "Michelin 1 Star",
     "tags": [
       "#미쉐린1스타",
@@ -324,11 +366,11 @@ const RESTAURANTS_DATA = [
     ]
   },
   {
-    "id": 7,
+    "id": 10,
     "category": "베트남만의 고급 로컬 다이닝",
     "name": "룩락 베트남 레스토랑",
     "englishName": "Luk Lak",
-    "shortIntro": "5성급 호텔 총주방장 출신의 셰프가 운영하는 세련된 로컬 다이닝",
+    "shortIntro": "5성급 호텔 총주방장 출신의 셰프가 운영하는 세련된 로컬 다이닝입니다. ...",
     "phone": "+84 943 143 686",
     "googleMapLink": "https://maps.google.com/?cid=4474371496991400046",
     "intro": "5성급 호텔 총주방장 출신의 셰프가 운영하는 세련된 로컬 다이닝입니다. 베트남 각 지역의 시그니처 요리와 소스들을 현대적인 레시피로 재해석하여 제공합니다.",
@@ -337,7 +379,7 @@ const RESTAURANTS_DATA = [
       "시그니처 허브 소스 닭고기 구이",
       "모던 스타일 짜까(가물치 구이)"
     ],
-    "rating": 4.0,
+    "rating": 4.3,
     "reviewsCount": "1,679",
     "tripAdvisorCount": "320",
     "address": "4A Lê Thánh Tông, Phan Chu Trinh, Hoàn Kiếm, Hà Nội",
@@ -377,11 +419,11 @@ const RESTAURANTS_DATA = [
     ]
   },
   {
-    "id": 8,
+    "id": 11,
     "category": "베트남만의 고급 로컬 다이닝",
     "name": "챕터 다이닝",
     "englishName": "Chapter Dining",
-    "shortIntro": "장작불과 숯의 불맛을 테마로 한 독창적이고 감각적인 그릴 코스 요리",
+    "shortIntro": "장작불과 숯을 주방의 핵심 테마로 삼아, 베트남 각지의 식재료를 창의적인...",
     "phone": "+84 333 201 221",
     "googleMapLink": "https://maps.google.com/?cid=9465291556293376904",
     "intro": "장작불과 숯을 주방의 핵심 테마로 삼아, 베트남 각지의 식재료를 창의적인 파인다이닝 코스로 풀어내는 레스토랑입니다. 미쉐린 가이드에도 선정되었으며 훌륭한 미식 경험을 제공합니다.",
@@ -390,11 +432,11 @@ const RESTAURANTS_DATA = [
       "장작구이 오리 가슴살",
       "숯불 해산물 스페셜"
     ],
-    "rating": 5.0,
+    "rating": 4.7,
     "reviewsCount": "305",
     "tripAdvisorCount": "70",
     "address": "12C Chân Cầm, Hàng Trống, Hoàn Kiếm, Hà Nội",
-    "hours": "오후 6:00~11:00",
+    "hours": "오후 6:00~오후 11:00",
     "michelinBadge": "Michelin Selected",
     "tags": [
       "#파인다이닝",
@@ -432,218 +474,30 @@ const RESTAURANTS_DATA = [
     ]
   },
   {
-    "id": 9,
-    "category": "한국인이 사랑하는 핫플레이스",
-    "name": "오바마 분짜 흐엉리엔",
-    "englishName": "Bún Chả Hương Liên",
-    "shortIntro": "오바마 전 대통령도 극찬한 하노이 필수 코스 숯불향 분짜 명소",
-    "phone": "+84 24 3943 4106",
-    "googleMapLink": "https://maps.google.com/?cid=9108413969528951015",
-    "intro": "오바마 전 미국 대통령이 방문하여 이른바 '오바마 분짜'로 세계적인 유명세를 탄 식당입니다. 숯불향이 가득 배어 있는 고기와 새콤달콤한 국물, 신선한 허브의 조화가 훌륭합니다.",
-    "menu": [
-      "오바마 콤보 (분짜+씨푸드 롤+하노이 맥주)",
-      "해산물 롤 (Nem Hải Sản)",
-      "게살 롤 (Nem Cua Bể)"
-    ],
-    "rating": 4.0,
-    "reviewsCount": "12,045",
-    "tripAdvisorCount": "2.1천",
-    "address": "24 Lê Văn Hưu, Phan Chu Trinh, Hai Bà Trưng, Hà Nội",
-    "hours": "오전 8:00~오후 8:00",
-    "michelinBadge": "Obama's Pick",
-    "tags": [
-      "#오바마분짜",
-      "#숯불구이",
-      "#하노이맥주",
-      "#가성비"
-    ],
-    "images": [
-      "https://lh3.googleusercontent.com/gps-cs-s/APNQkAEgsuPTGv9ChJbnUOlI5kayrdHcimJ_-f64cXlWKlLnYA3dbp4hoxmJxTTIYEEVh67Slik3WoEXypFCU2eLq62Em7-2YMRQJ8uDW_yku1JVQni559Ij5Tb2RozaqZYzVAJ4EDvV=s1360-w1360-h1020-rw",
-      "https://pimg.mk.co.kr/news/cms/202304/24/news-p.v1.20230424.3b65a212c8a24229843c844c0f75841a.png",
-      "https://lh3.googleusercontent.com/gps-cs-s/APNQkAH8w65TwtQ5o8pg4gJnmVpW1jJLmkUQ2tDpuVXTwX89ZEJLV3-WDZyCG5wkMEMrPb9rF6sjxIPVNzO1wKcQ5khhF7lvowarHpzRcgz91sSmPjjPnLYJ-iKKl3QQ_E_vV2x8262opLuO-_ox=s1360-w1360-h1020-rw",
-      "https://lh3.googleusercontent.com/gps-cs-s/APNQkAHcTEmuA02pNBTfUkzUww5H8VJwa5GAoYD8RZU3VfUAqQTpFJFYhYRMPPwCljibuuQw-trcaZ2HSOrn2_FrZ6ql2nnO_NFGzJwY69u_f-lrKQeFc5681BhP1ncJ4M5qCLVt-cOPVz4BMZ3s=w408-h544-k-no",
-      "https://lh3.googleusercontent.com/gps-cs-s/APNQkAGUye7cu8Syx21a3d_bqhVtzwn3MZkMKyHNmKN1sbQ8B4nnXN1qQpISCywu9joUUxkTKL4IRNSUpIFPOv49Czwl6ZvkXr87NSV9Rms2BEEuSn_YMxYAMwVcAdMvmwJpiy7SNr0pbt-sLHb3=s1360-w1360-h1020-rw"
-    ],
-    "reviews": [
-      {
-        "username": "오바마따라잡기",
-        "rating": 5,
-        "text": "콤보 세트를 시켰는데 분짜 국물의 새콤달콤함과 고기 숯불 향이 예술입니다. 게살 롤은 속이 꽉 차서 맥주 안주로 최고예요. 오바마 대통령 자리가 그대로 보존되어 있어 신기했습니다.",
-        "date": "2026-05-25"
-      },
-      {
-        "username": "분짜홀릭",
-        "rating": 4,
-        "text": "진한 숯불향 고기와 차가운 쌀국수 생면을 같이 싸서 소스에 푹 찍어 드시면 아주 든든합니다. 하노이에 오면 꼭 와봐야 할 명소입니다.",
-        "date": "2026-05-18"
-      },
-      {
-        "username": "위생은흐림",
-        "rating": 4,
-        "text": "로컬 식당이다 보니 매우 깔끔한 위생은 아니지만 현지 로컬 맛집 분위기를 그대로 체감할 수 있습니다. 고기 양도 참 푸짐하고 좋았습니다.",
-        "date": "2026-05-15"
-      }
-    ]
-  },
-  {
-    "id": 10,
-    "category": "한국인이 사랑하는 핫플레이스",
-    "name": "분짜닥킴",
-    "englishName": "Bún Chả Đắc Kim",
-    "shortIntro": "1966년부터 하노이 올드쿼터를 지켜온 묵직한 숯불향과 바삭한 넴란",
-    "phone": "+84 24 3828 7060",
-    "googleMapLink": "https://maps.app.goo.gl/WGQmbVAegp1XSGXs8",
-    "intro": "1966년에 설립된 하노이 올드쿼터의 유서 깊은 분짜 전문점입니다. 진한 숯불 향의 돼지고기와 바삭한 대형 넴(Nem)이 특징이며, 현지인과 관광객 모두에게 꾸준히 사랑받는 곳입니다.",
-    "menu": [
-      "분짜 (Bún Chả)",
-      "넴 란 (Nem Rán - 게살 튀김 만두)"
-    ],
-    "rating": 4.0,
-    "reviewsCount": "7,983",
-    "tripAdvisorCount": "650개",
-    "address": "1 Hàng Mành, Hàng Gai, Hoàn Kiếm, Hà Nội",
-    "hours": "오전 9:00~오후 9:00",
-    "michelinBadge": "Traditional Taste",
-    "tags": [
-      "#올드쿼터분짜",
-      "#바삭한넴란",
-      "#역사적인맛집",
-      "#고기듬뿍"
-    ],
-    "images": [
-      "https://lh3.googleusercontent.com/gps-cs-s/APNQkAE8k_r2nnSkf2wYrVtydetJab2XWfxA2goHgR-fqFbOyEuhrOWKFowl_oNwZLoKtJdAorfbJonhybyK42FJi6SO4AAbFjNyLoKdKTG3MR0qH9E-rOKdWhTIChQ728U1uVPLRoCRR3EXSzLo=w243-h174-n-k-no-nu",
-      "https://lh3.googleusercontent.com/gps-cs-s/APNQkAFXjSamhzae_LlK3G82CEuGyhtx3FEV-uNpiHtlsJi2wlp3w48KY8TUuOZtDgUOadCeMK6T5TydmxO3-OAAj4_H4wIInZveI0gga2MF3VUW5dTMjB55rGYCioDc5xtraHjOuTe69ikfgPP8=w243-h174-n-k-no-nu",
-      "https://ak-d.tripcdn.com/images/100t0h0000008v3ek589A_C_760_506_R5.jpg?proc=autoorient",
-      "https://ak-d.tripcdn.com/images/0102c12000fn1gs5564DE_C_760_506_R5.jpg?proc=autoorient",
-      "https://ak-d.tripcdn.com/images/1009040000000azsc557C_C_760_506_R5.jpg?proc=autoorient"
-    ],
-    "reviews": [
-      {
-        "username": "분짜투어러",
-        "rating": 5,
-        "text": "양념 소스가 적절히 시큼해서 한국인 입맛에 아주 잘 맞습니다. 넴란 튀김 크기가 주먹만 한데 속이 다진 고기와 꽃게살로 꽉 차서 겉바속촉 그 자체네요.",
-        "date": "2026-05-24"
-      },
-      {
-        "username": "맛동요정",
-        "rating": 4,
-        "text": "1층부터 3층까지 자리가 넉넉합니다. 분짜 육수에 다진 마늘과 고추를 듬뿍 넣고 숯불 고기와 야채를 적셔 드셔보세요. 감칠맛이 최고입니다.",
-        "date": "2026-05-12"
-      },
-      {
-        "username": "전통의맛",
-        "rating": 4,
-        "text": "하노이 올드쿼터 골목 안에서 가장 오래된 유명 분짜 가게 중 하나입니다. 고기 향이 아주 묵직하고 푸짐해서 든든하게 먹고 여행을 다녔습니다.",
-        "date": "2026-05-05"
-      }
-    ]
-  },
-  {
-    "id": 11,
-    "category": "한국인이 사랑하는 핫플레이스",
-    "name": "퍼텐리궉수",
-    "englishName": "Pho 10 Ly Quoc Su",
-    "shortIntro": "미쉐린 빕구르망이 인정한 맑고 깊은 감칠맛의 소고기 쌀국수 성지",
-    "phone": "+84 24 3825 7338",
-    "googleMapLink": "https://maps.app.goo.gl/7ht6qtwrAn8ojmH59",
-    "intro": "하노이에서 가장 유명한 소고기 쌀국수 맛집 중 하나로, 미쉐린 빕 구르망에 선정되었습니다. 맑으면서도 깊은 감칠맛을 내는 육수가 일품이며, 기호에 따라 튀김 빵인 '꿔이'를 국물에 적셔 먹는 것이 별미입니다.",
-    "menu": [
-      "퍼 찐 (Phở Chín - 완숙 소고기 쌀국수)",
-      "퍼 따이 (Phở Tái - 레어 소고기 쌀국수)",
-      "퍼 따이 란 (볶은 소고기 쌀국수)"
-    ],
-    "rating": 4.0,
-    "reviewsCount": "11,572",
-    "tripAdvisorCount": "1.2천",
-    "address": "10 Lý Quốc Sư, Hàng Trống, Hoàn Kiếm, Hà Nội",
-    "hours": "오전 6:00~오후 10:00",
-    "michelinBadge": "Bib Gourmand",
-    "tags": [
-      "#인생쌀국수",
-      "#소고기쌀국수",
-      "#미쉐린빕구르망",
-      "#현지인추천"
-    ],
-    "images": [
-      "https://lh3.googleusercontent.com/gps-cs-s/APNQkAGHP2FwaDpCNL0HaBK-jib-16HegJzKCq9zk4N58rbg1m97cQcn5KzPqyuARhZKMB2D9uAjfA0x0C-gzN-aWpGgUdoy74y_05DPaWaGJvZyZseSc-XgZqZ6A_QBXQE4iZLj-WI=w408-h544-k-no",
-      "https://pho10lyquocsu.com.vn/watermark/product/540x540x1/upload/product/z4505622509262e7a97d75dd1cde5276fe249e8206669b-6819.jpg",
-      "https://pho10lyquocsu.com.vn/thumbs/614x474x1/upload/news/2476e204f0f40eaa57e5-6513.jpg",
-      "https://pho10lyquocsu.com.vn/thumbs/632x960x2/upload/product/z2343701228006e30ed09d5e4e8756b8496e754e523f71-6130.jpg"
-    ],
-    "reviews": [
-      {
-        "username": "쌀국수킬러",
-        "rating": 5,
-        "text": "하노이에서 가본 쌀국수집 중에 육수가 가장 맑고 깔끔하면서 깊은 국물 맛이었습니다. 야들야들한 소고기도 한 가득 올라가 있어요.",
-        "date": "2026-05-26"
-      },
-      {
-        "username": "쌀국수도전",
-        "rating": 4,
-        "text": "현지인들처럼 꿔이(튀김빵)를 주문해서 국물에 말아먹어 보았습니다. 꿔이 속으로 육수가 흠뻑 스며들어 별미네요. 필수 추가 추천합니다.",
-        "date": "2026-05-19"
-      },
-      {
-        "username": "빠른회전",
-        "rating": 5,
-        "text": "웨이팅이 조금 길지만 쌀국수 단일 테이블이라 회전율이 엄청나게 빠릅니다. 고춧가루를 조금 풀면 해장용 국물로 이만한 게 없네요.",
-        "date": "2026-05-10"
-      }
-    ]
-  },
-  {
     "id": 12,
-    "category": "한국인이 사랑하는 핫플레이스",
-    "name": "분보남보백프응",
-    "englishName": "Bún Bò Nam Bộ Bách Phương",
-    "shortIntro": "새콤달콤한 특제 소스와 소고기를 비벼 먹는 하노이 비빔 쌀국수",
-    "phone": "+84 911 529 166",
-    "googleMapLink": "https://maps.app.goo.gl/XyT37pJiM3b7T6yD9",
-    "intro": "하노이 스타일의 비빔 쌀국수인 '분보남보'를 전문으로 하는 곳입니다. 볶은 소고기, 숙주, 땅콩, 튀긴 양파 등이 소스와 어우러져 고소하고 새콤달콤한 맛을 선사하며, 가벼우면서도 든든한 한 끼 식사로 제격입니다.",
+    "category": "베트남만의 고급 로컬 다이닝",
+    "name": "카우고 베트남 퀴진",
+    "englishName": "Cau Go Vietnamese Cuisine",
+    "shortIntro": "호안끼엠 호수 전망과 함께 정갈하고 고급스러운 베트남 전통 요리를 즐길 ...",
+    "phone": "+84 838332688",
+    "googleMapLink": "https://maps.app.goo.gl/HZCAj9k6xU8Jv8t37",
+    "intro": "호안끼엠 호수 전망과 함께 정갈하고 고급스러운 베트남 전통 요리를 즐길 수 있는 곳입니다.",
     "menu": [
-      "분보남보 (Bún Bò Nam Bộ)",
-      "넴 쭈아 잔 (Nem Chua Rán - 튀긴 돼지고기)",
-      "바잉 바오 (Bánh Bao)"
+      "연잎 밥",
+      "해산물 볶음",
+      "베트남식 생선 구이"
     ],
-    "rating": 4.0,
-    "reviewsCount": "7,655",
-    "tripAdvisorCount": "980개",
-    "address": "67 Hàng Điếu, Cửa Đông, Hoàn Kiếm, Hà Nội",
-    "hours": "오전 7:30~오후 10:30",
-    "michelinBadge": "Noodle Master",
-    "tags": [
-      "#비빔쌀국수",
-      "#고소한땅콩",
-      "#한그릇뚝딱",
-      "#깔끔실내"
-    ],
-    "images": [
-      "https://blog.kakaocdn.net/dna/kFsi7/btsL9c1PRdT/AAAAAAAAAAAAAAAAAAAAAFm_JVFgzRJJWzGMHcg19Px2-Wwgcb9bn6qV6jDvpmaW/img.jpg?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&expires=1780239599&allow_ip=&allow_referer=&signature=8nTWS8%2BKVZCKMZBucjZQNmAVy2Q%3D",
-      "https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/c255d928-4d74-4fb2-a361-ec96d36feb86.jpeg",
-      "https://mblogthumb-phinf.pstatic.net/MjAyMzEyMjdfMTc2/MDAxNzAzNjQ1Njk4MjUx.0wJyvl92KJsoyw7HqsT3KY3JCC3Om2g6FLaQrnqkuU4g.qY4Wy9GgFdX72y5yVhQvVS7XYWFC6Ltdv8xHsDfXZxQg.JPEG.seoavo/output_422291186.jpg?type=w800"
-    ],
-    "reviews": [
-      {
-        "username": "맛동그리",
-        "rating": 5,
-        "text": "국물이 없는 비빔 국수인데 볶아진 고소한 땅콩과 숙주가 새콤달콤한 소스와 섞이면서 입안 가득 환상적인 맛을 줍니다. 한국식 간장 불고기 비빔면 느낌도 나서 호불호가 없어요.",
-        "date": "2026-05-24"
-      },
-      {
-        "username": "깔끔매장",
-        "rating": 4,
-        "text": "하노이 비빔 쌀국수의 진수를 볼 수 있습니다. 현지 로컬 맛집들 중 실내가 정말 깨끗하고 대형 에어컨이 잘 나와서 쾌적한 식사가 가능했습니다.",
-        "date": "2026-05-17"
-      },
-      {
-        "username": "맥주안주",
-        "rating": 5,
-        "text": "메인 국수 외에 사이드로 파는 넴쭈아잔(튀김 소시지)이 참 쫄깃합니다. 맥주 한 병 같이 시켜서 한 그릇 비우기에 딱 좋은 캐주얼 식당입니다.",
-        "date": "2026-05-11"
-      }
-    ]
+    "rating": 4.4,
+    "reviewsCount": "2,589",
+    "tripAdvisorCount": "",
+    "address": "",
+    "hours": "오전 10:00~오후 11:00",
+    "michelinBadge": "",
+    "tags": [],
+    "images": [],
+    "reviews": []
   },
-{
+  {
     "id": 13,
     "category": "베트남만의 고급 로컬 다이닝",
     "name": "홈 하노이 레스토랑",
@@ -1194,7 +1048,7 @@ const RESTAURANTS_DATA = [
     "images": [],
     "reviews": []
   }
-];
+]];
 
 const PHRASES_DATA = [
   { kr: "안녕하세요", vn: "Xin chào", ko: "씬 짜오" },
